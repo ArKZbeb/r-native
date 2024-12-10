7f5fe95700f3fb9a1c973f98279a17ece7e30a01
justoverfr <jeanjuste.lefebvre@gmail.com>
2024-12-10 01:24:20 +0100

    chore: install React Native Elements  dependencies

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,5 +1,18 @@
 import { Stack } from "expo-router";
+import { createTheme, ThemeProvider } from "@rneui/themed";
+import { SafeAreaProvider } from "react-native-safe-area-context";
+
+const theme = createTheme({
+  lightColors: {},
+  darkColors: {},
+});
 
 export default function RootLayout() {
-  return <Stack />;
+  return (
+    <SafeAreaProvider>
+      <ThemeProvider theme={theme}>
+        <Stack />
+      </ThemeProvider>
+    </SafeAreaProvider>
+  );
 }
